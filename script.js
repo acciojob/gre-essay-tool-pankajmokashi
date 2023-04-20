1db0e42f5da8761e6e53fcbdbe97da71f49b8ec1
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-20 19:04:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+let input = document.getElementById("evaluatedText")
+let h3 = document.getElementById("wordCount")
+h3.innerText = 0
+function textInside() {
+    let line = input.value
+    let arr = line.trim()
+    arr = arr.split(" ")
+    arr = arr.filter(function(item) {
+        return item !== ''
+    })
+    h3.innerText = arr.length
+}
+input.addEventListener("keyup", textInside)
